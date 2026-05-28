4f8bb8b43da1b02cddf6922907dcca2efac8a21c
marmol89 <38738460+marmol89@users.noreply.github.com>
2026-05-28 18:10:46 +0200

    (update) action github and pre commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,47 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate', // Actualiza el Service Worker automáticamente sin recargar
-      includeAssets: ['*'],
+      registerType: "autoUpdate", // Actualiza el Service Worker automáticamente sin recargar
+      includeAssets: ["*"],
       manifest: {
-        name: 'Mi Aplicación Increíble',
-        short_name: 'MiPWA',
-        description: 'Una app ultra rápida hecha con Vite, PWA y pnpm',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
+        name: "Mi Aplicación Increíble",
+        short_name: "MiPWA",
+        description: "Una app ultra rápida hecha con Vite, PWA y pnpm",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable' // Clave para que el icono se adapte bien en Android
-          }
-        ]
-      }
-    })
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", // Clave para que el icono se adapte bien en Android
+          },
+        ],
+      },
+    }),
   ],
-  server : {
-    host : true,
-    port : 5173,
-    watch : {
-      usePolling: true
-    }
-  }
-})
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
