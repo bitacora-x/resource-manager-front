ab82121457fdadee6b5097ad2d2c5a34ae4b1672
sikay <pedrohdezmora@gmail.com>
2026-05-28 19:09:25 +0200

    added first test

---
+++ test/LandingPage.test.ts
@@ -0,0 +1,9 @@
+import { it } from 'vitest';
+import {render, fireEvent, screen} from '@testing-library/vue';
+import HelloWorld from "../src/components/HelloWorld.vue";
+
+it('creates a user with the correct fields', () => {
+    render(HelloWorld);
+
+    screen.getByText('Count is 0');
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
@@ -43,5 +43,8 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+  },
+  test: {
+    environment: 'happy-dom',
   },
 });
